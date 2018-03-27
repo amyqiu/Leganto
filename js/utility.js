62a0a296dbb69d71a40fadb39b21315c0ccdfddc
Amy Qiu <amy-qiu@outlook.com>
2018-03-26 21:28:47 -0400

    Fixed status code

--- js/utility.js
+++ js/utility.js
@@ -59,7 +59,7 @@ function endpointPostRequest(url, callback, body, id) {
     }
 
     http.onreadystatechange = function() {//Call a function when the state changes.
-        if(http.readyState == 4 && http.status == 200) {
+        if(http.readyState == 4 && http.status == 201) {
             callback();
         } else if (http.readyState == 4){
           console.log("Error has occurred in post request");
