928b271017f4118b3ae887445de0d5d4a7254462
Amy Qiu <amy-qiu@outlook.com>
2018-03-27 13:43:44 -0400

    Add button for adding new bookshelf

---
+++ js/addbookshelf.js
@@ -0,0 +1,15 @@
+function createBookshelf(){
+  var name = document.getElementById("name").value;
+  var books = [];
+
+  var bookshelf = {
+    name,
+    books
+  }
+
+  endpointPostRequest("http://localhost:8080/bookshelf/post/", redirect, bookshelf)
+}
+
+function redirect(){
+  window.location.href = "dashbaord.html";
+}
